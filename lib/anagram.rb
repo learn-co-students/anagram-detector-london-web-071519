f5936c3794854ea5dc505b6df0a533d07376e0c0
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-21 16:21:08 +0100

    anagram complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+        # binding.pry
+    end
+
+    def match(words)
+        words.select {|word| @word.split(/\.|/).sort == word.split(/\.|/).sort}
+    end
+
+end
