a5722538e194220f5f61cd6b841962d0083e148f
Harry Chopra <harrychopra@gmail.com>
2019-07-28 16:44:02 +0100

    Lab Solved

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+attr_accessor :input
+
+def initialize(input)
+    @input = input
+end
+
+def match(arr)
+    arr.select{|word| word.split("").sort == @input.split("").sort}
+end
+
+
+
+end
+
